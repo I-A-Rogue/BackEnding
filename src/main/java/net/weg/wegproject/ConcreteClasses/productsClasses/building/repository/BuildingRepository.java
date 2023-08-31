4c8c588ca3b6a6a11f65757ce9cd48511b77ea9e
Vitor Manoel Alves de Andrade <vitorgml21@gmail.com>
2023-08-31 15:08:33 -0300

    att

--- src/main/java/net/weg/wegproject/ConcreteClasses/productsClasses/building/repository/BuildingRepository.java
+++
@@ -1,9 +0,0 @@
-package net.weg.wegproject.ConcreteClasses.productsClasses.building.repository;
-
-import net.weg.wegproject.ConcreteClasses.productsClasses.building.model.Building;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BuildingRepository extends JpaRepository<Building, Long> {
-}
