05fe1663481fb5876269c2a135789cb357b40a4d
Vitor Manoel Alves de Andrade <vitorgml21@gmail.com>
2023-11-28 16:15:23 -0300

    att

--- src/main/java/net/weg/wegproject/security/controller/AutenticationController.java
+++ src/main/java/net/weg/wegproject/security/controller/AutenticationController.java
@@ -11,10 +11,12 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
+@CrossOrigin
 @AllArgsConstructor
 public class AutenticationController {
 
