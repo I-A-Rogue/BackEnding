8022b0a08e197437aa213fbeeef8af638423a7f3
Inácio Z. Vitkoski <65171054+inaciozacko@users.noreply.github.com>
2023-12-05 20:26:55 -0300

    Att

--- src/main/java/net/weg/wegproject/ConcreteClasses/productsClasses/product/repository/ProductRepository.java
+++ src/main/java/net/weg/wegproject/ConcreteClasses/productsClasses/product/repository/ProductRepository.java
@@ -4,10 +4,8 @@ import net.weg.wegproject.ConcreteClasses.productsClasses.product.model.entity.F
 import net.weg.wegproject.ConcreteClasses.productsClasses.product.model.entity.Product;
 import net.weg.wegproject.ConcreteClasses.productsClasses.product.model.projections.ProductResum;
 import net.weg.wegproject.enums.Category;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
